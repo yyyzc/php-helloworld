e329d644d17f9a62a861f58375e6efbc27c24e12
liuxiaoyu-git <xiaoyliu@redhat.com>
2022-01-17 10:59:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 print "Hello World !!! PHP version is " . PHP_VERSION . "\n";
 ?>
+
+
+<?php 
+print "REMOTE_ADDR:";
+$ip=$_SERVER["REMOTE_ADDR"];
+echo $ip;
+?> 
+
+<?php
+print "$user_IP:";
+$user_IP = ($_SERVER["HTTP_VIA"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : $_SERVER["REMOTE_ADDR"];
+$user_IP = ($user_IP) ? $user_IP : $_SERVER["REMOTE_ADDR"];  
+echo $user_IP;
+?>
