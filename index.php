92f388d0fdfaca3c49aa57db9a128eb3c52ccd1d
liuxiaoyu-git <xiaoyliu@redhat.com>
2022-01-17 19:13:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,14 @@ echo $ip;
 ?> 
 
 <br>
-
-
+<?php
+print "$user_IP:";
+$user_IP = ($_SERVER["HTTP_VIA"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : $_SERVER["REMOTE_ADDR"];
+$user_IP = ($user_IP) ? $user_IP : $_SERVER["REMOTE_ADDR"];  
+echo $user_IP;
+?>
+<br>
+<br>
 <?php
 $headers = apache_request_headers();
 
